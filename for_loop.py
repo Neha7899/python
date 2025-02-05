0977518993edb6ca8e2fa3b556c5e7139096621a
neha7899 <anehareddy7@gmail.com>
2025-02-05 18:38:23 +0530

    for loop

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+"""for loop"""
+"""for loop to print the elements in a list"""
+
+fruits=["apple","banana","cherry","dragonfruit"]
+for fruit in fruits:
+    print(fruit)
+
+
+"""length of a list"""
+
+fruits=["apple","banana","cherry","dragonfruit"]
+for idx in range(0,len(fruits)):
+    print(len(fruits))
+    print(idx,fruits[idx])
