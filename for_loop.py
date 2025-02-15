501690b682b2d07ebf5e395a8eaff69cdc3bdbba
neha7899 <anehareddy7@gmail.com>
2025-02-15 19:47:19 +0530

    for loop-looping statement

--- for_loop.py
+++ for_loop.py
@@ -6,9 +6,14 @@ for fruit in fruits:
     print(fruit)
 
 
-"""length of a list"""
+"""length of a list and index of a list"""
 
 fruits=["apple","banana","cherry","dragonfruit"]
 for idx in range(0,len(fruits)):
     print(len(fruits))
     print(idx,fruits[idx])
+
+
+
+
+
