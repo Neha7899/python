592741aca44c33c054d22d1eebe4f6c01176e5e4
neha7899 <anehareddy7@gmail.com>
2025-02-24 19:50:22 +0530

    scope of a variable 1.global variable 2.local variable

---
+++ scope.py
@@ -0,0 +1,19 @@
+a=2     #global variable
+def fn():
+    return a+2
+print(fn())
+
+
+def fn():
+    b=10        #local variable
+    return b-2    #using it through return
+print(fn())
+
+
+a=10
+def fn():
+    global b
+    b=10
+    return a+10,b+2
+print(fn())
+print(b)
